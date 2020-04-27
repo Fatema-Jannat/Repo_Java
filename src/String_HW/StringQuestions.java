075da959e6c2beef1647c56de9a718d2c4d221a6
Fatema Jannat <jannat.fatema027@gmail.com>
2020-04-27 17:03:52 -0400

    First homework

---
+++ src/String_HW/StringQuestions.java
@@ -0,0 +1,8 @@
+package String_HW;
+
+public class StringQuestions {
+    public static void main(String[] args) {
+        String name = "Peace required";
+        System.out.println("Message is " + name);
+    }
+}
