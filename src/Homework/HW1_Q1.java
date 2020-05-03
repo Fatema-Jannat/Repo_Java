c8674026042a0d92549ce02a2dfcf3d9d06d3281
Fatema Jannat <jannat.fatema027@gmail.com>
2020-05-03 01:40:57 -0400

    First homework

--- src/Homework/HW1_Q1.java
+++ src/Homework/HW1_Q1.java
@@ -14,24 +14,24 @@ package Homework;
  */
 public class HW1_Q1 {
     public static void main(String[] args) {
-        int numOfHours = 24; //Total number of hours in a day
-        int numOfDays = 365; //Total number of max no. of days in a years
+        byte numOfHours = 24; //Total number of hours in a day
+        short numOfDays = 365; //Total number of max no. of days in a years
         int totalNumOfEmpl = 50; //total number of employees in an organisation
-        double population = 328.2; //population in a country
-        double interestRate = 18.23; //interest rate
+        long population = 1400000000L; //population in a country
+        float interestRate = 18.23f; //interest rate
         double accBalance = 5000.00; //balance in a bank account
         char initial1 = 'F'; //First Initial
         char initial2 = 'J'; //Last Initial
         String name = "Fatema Jannat"; //Full name
-        String sunRise = "No, The sun rises in the east and set in the west."; //Answer to #7 Question
+        boolean sunRise = false; //Answer to #7 Question
 
         System.out.println("1. Number of hours in a day: " + numOfHours + " hours.");
         System.out.println("2. Number of max no. of days in a years: " +numOfDays +" days.");
         System.out.println("3. Total number of employees in an organisation: " + totalNumOfEmpl);
-        System.out.println("4. Population in a country: " + population + " million.");
+        System.out.println("4. Population in a country: " + population + " billion.");
         System.out.println("5. Interest Rate: " + interestRate +"%");
         System.out.println("6. Balance in a bank account: $"+ accBalance);
-        System.out.println("7. Does the sun rise from the west?"+ "\n" + sunRise);// "\n" means new line
+        System.out.println("7. Does the sun rise from the west? " + sunRise);
         System.out.println("8. Initials of your name: "+ initial1 + initial2);
         System.out.println("9. Your name: " + name);
 
--- src/String_HW/StringQuestions.java
+++
@@ -1,8 +0,0 @@
-package String_HW;
-
-public class StringQuestions {
-    public static void main(String[] args) {
-        String name = "Peace required";
-        System.out.println("Message is " + name);
-    }
-}
